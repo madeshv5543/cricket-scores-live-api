1622414539e687c0a8c476fb33a8f3c8cdf24e89
ChrisDobby <dobby@chrisjdobson.co.uk>
2019-04-19 19:46:36 +0100

    Deploy functions using serverless

--- src/lambdas/match/getMatch.js
+++ src/lambdas/match/getMatch.js
@@ -2,7 +2,22 @@ import util from 'util';
 import mongoDb from '../../db/mongo';
 import inMemoryDb from '../../db/inMemory';
 
-const getMatch = db => async event => util.promisify(db.get)(event.id);
+const getMatch = db => async event => {
+    const id = event.pathParameters.id;
+    const match = await util.promisify(db.get)(id);
+
+    if (!match) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify(`Match with id ${id} does not exist`),
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(match),
+    };
+};
 
 exports.handler = getMatch(
     process.env.IN_MEMORY ? inMemoryDb : mongoDb(process.env.MONGO_CONNECTION, () => new Date()),
--- src/lambdas/match/getMatches.js
+++ src/lambdas/match/getMatches.js
@@ -2,7 +2,14 @@ import util from 'util';
 import mongoDb from '../../db/mongo';
 import inMemoryDb from '../../db/inMemory';
 
-const getMatches = db => async event => util.promisify(db.getAll)(event.query);
+const getMatches = db => async event => {
+    const matches = await util.promisify(db.getAll)(event.query);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(matches),
+    };
+};
 
 exports.handler = getMatches(
     process.env.IN_MEMORY ? inMemoryDb : mongoDb(process.env.MONGO_CONNECTION, () => new Date()),
