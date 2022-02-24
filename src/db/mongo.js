2ba16d1d37a4e09969d9b7dfddb34a5fb2342ac7
xAdministrator <xAdministrator@BalakrishnanM1.ad.local>
2022-02-24 20:24:48 +0530

    mongo changes

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -6,7 +6,7 @@ const maxDaysForMatch = 6;
 /* eslint-disable no-console */
 export default (connection, getDate) => {
     let db;
-    MongoClient.connect(connection, { useNewUrlParser: true,useUnifiedTopology:true }, (err, database) => {
+    MongoClient.connect(connection, { useNewUrlParser: true}, (err, database) => {
         if (err) {
             console.error(err);
             return;
